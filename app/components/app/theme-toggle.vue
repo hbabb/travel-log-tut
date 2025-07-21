6e86eeeb86d6d75dfa4bf8753e13a1ffdffd6374
Heath Babb <hbabb@heath-babb.com>
2025-07-21 17:12:43 -0400

    :sparkles: add theme toggle and set themes (#51)

---
+++ app/components/app/theme-toggle.vue
@@ -0,0 +1,27 @@
+<!-- <script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+v-model="isDark"
+-->
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <!-- this hidden checkbox controls the state -->
+    <input type="checkbox" class="theme-controller" value="business">
+
+    <!-- sun icon -->
+    <Icon class="swap-off" name="tabler:sun" size="24" />
+
+    <!-- moon icon -->
+    <Icon class="swap-on" name="tabler:moon-filled" size="24" />
+  </label>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero bg-base-200 container mx-auto mt-4">
+  <section class="hero bg-base-300 container mx-auto mt-4">
     <article class="hero-content text-center min-h-96">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">
