a67f850a30af558a086568230e6e16754d49240e
Heath Babb <hbabb@heath-babb.com>
2025-07-21 12:49:12 -0400

    :sparkles: add landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="navbar bg-primary text-primary-content">
+    <nav class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </nav>
+    <nav class="navbar-end">
+      <a class="btn btn-accent">
+        Sign In With GitHub
+        <Icon name="tabler:brand-github" size="24" />
+      </a>
+    </nav>
+  </header>
+</template>
