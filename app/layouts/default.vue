52160ade025e9a541688085172ecdbd6758326cc
Heath Babb <hbabb@heath-babb.com>
2025-07-21 12:52:12 -0400

    ✨ add landing page (#50)
    * :sparkles: setup tailwind / daisy ui / icons
    
    * :sparkles: add landing page

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-dvh flex-col">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="hero bg-base-200 container mx-auto mt-4">
+    <article class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With GitHub
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </article>
+  </section>
+</template>
