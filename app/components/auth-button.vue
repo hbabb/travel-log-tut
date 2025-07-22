4039d1c0055c75b66cec25723a20574588a3313b
Heath Babb <hbabb@heath-babb.com>
2025-07-22 17:32:44 -0400

    :sparkles: add auth state / sign out

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -3,7 +3,25 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn bg-black text-white border-black" @click="authStore.signIn">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out" class="text-black">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.loading" class="btn bg-black text-white border-black" @click="authStore.signIn">
     <span v-if="authStore.loading" class="loading loading-ring loading-md text-warning" />
     <Icon v-else name="tabler:brand-github" size="24" />
     Sign In With GitHub
