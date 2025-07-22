30b1a8043347425e856af41c70d5d89640fd8745
Heath Babb <hbabb@heath-babb.com>
2025-07-21 21:28:33 -0400

    :bug: revised mode toggle to work with system preference

--- app/components/app/theme-toggle.vue
+++ app/components/app/theme-toggle.vue
@@ -1,7 +1,20 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
 <template>
   <label class="swap swap-rotate mx-4">
     <!-- this hidden checkbox controls the state -->
-    <input type="checkbox" class="theme-controller" value="business">
+    <input v-model="isDark" type="checkbox" class="theme-controller" value="business">
 
     <!-- sun icon -->
     <Icon class="swap-off" name="tabler:sun" size="24" />
