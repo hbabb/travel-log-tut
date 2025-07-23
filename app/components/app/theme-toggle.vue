5d44659c1ec0e4e7043202e1696f65e05af88b0a
Heath Babb <hbabb@heath-babb.com>
2025-07-23 10:20:55 -0400

    :sparkles: fixes #21 / create collapaible sidebar with smooth animation (#59)

--- app/components/app/theme-toggle.vue
+++ app/components/app/theme-toggle.vue
@@ -14,7 +14,7 @@ const isDark = computed({
 <template>
   <label class="swap swap-rotate mx-4">
     <!-- this hidden checkbox controls the state -->
-    <input v-model="isDark" type="checkbox" class="theme-controller" value="night">
+    <input v-model="isDark" type="checkbox" class="theme-controller" value="business">
 
     <!-- sun icon -->
     <Icon class="swap-off" name="tabler:sun" size="24" />
