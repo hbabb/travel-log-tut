c063fcf64a4448a6849febf1fec383d53ef1bc67
Heath Babb <hbabb@heath-babb.com>
2025-07-24 17:07:16 -0400

    Improve authentication middleware with user context

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,10 +1,12 @@
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
+  // @ts-expect-error Better Auth user id generation is off and the DB is set to use number not string
+  event.context.user = session?.user;
   if (event.path.startsWith("/dashboard")) {
-    const session = await auth.api.getSession({
-      headers: event.headers,
-    });
     if (!session?.user) {
       await sendRedirect(event, "/", 302);
     }
